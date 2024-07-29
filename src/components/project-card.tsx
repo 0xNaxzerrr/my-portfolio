bf0fc9b18febf2eadb29f513a6a327f93d1e9976
0xNazerrr <lattana-s@hotmail.fr>
2024-07-29 23:09:52 +0200

    :fire: final

--- src/components/project-card.tsx
+++ src/components/project-card.tsx
@@ -64,6 +64,8 @@ export function ProjectCard({
           <Image
             src={image}
             alt={title}
+            width={640}
+            height={360}
             className="h-40 w-full overflow-hidden object-cover object-top"
           />
         )}
